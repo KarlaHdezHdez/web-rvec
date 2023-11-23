789635762faa1257faff2248d0cf25ae5bdbdef1
perla0212 <115654448+perla0212@users.noreply.github.com>
2023-11-23 10:25:29 -0600

    Update deploy_laravel.sh

--- script/deploy_laravel.sh
+++ script/deploy_laravel.sh
@@ -4,36 +4,36 @@
 cd /var/www/web_rvec/
 
 # Create cache and chmod folders
-mkdir -p /var/www/html/bootstrap/cache
-mkdir -p /var/www/html/storage/framework/sessions
-mkdir -p /var/www/html/storage/framework/views
-mkdir -p /var/www/html/storage/framework/cache
-mkdir -p /var/www/html/public/files/
+mkdir -p /var/www/web_rvec/bootstrap/cache
+mkdir -p /var/www/web_rvec/storage/framework/sessions
+mkdir -p /var/www/web_rvec/storage/framework/views
+mkdir -p /var/www/web_rvec/storage/framework/cache
+mkdir -p /var/www/web_rvec/public/files/
 
 # Install dependencies
 export COMPOSER_ALLOW_SUPERUSER=1
-composer install -d /var/www/html/
+composer install -d /var/www/web_rvec/
 
 # Copy configuration from /var/www/.env, see README.MD for more information
-cp /var/www/.env /var/www/html/.env
+cp /var/www/.env /var/www/web_rvec/.env
 
 # Migrate all tables
-php /var/www/html/artisan migrate
+php /var/www/web_rvec/artisan migrate
 
 # Clear any previous cached views
-php /var/www/html/artisan config:clear
-php /var/www/html/artisan cache:clear
-php /var/www/html/artisan view:clear
+php /var/www/web_rvec/artisan config:clear
+php /var/www/web_rvec/artisan cache:clear
+php /var/www/web_rvec/artisan view:clear
 
 # Optimize the application
-php /var/www/html/artisan config:cache
-php /var/www/html/artisan optimize
+php /var/www/web_rvec/artisan config:cache
+php /var/www/web_rvec/artisan optimize
 #php /var/www/html/artisan route:cache
 
 # Change rights
-chmod 777 -R /var/www/html/bootstrap/cache
-chmod 777 -R /var/www/html/storage
-chmod 777 -R /var/www/html/public/files/
+chmod 777 -R /var/www/web_rvec/bootstrap/cache
+chmod 777 -R /var/www/web_rvec/storage
+chmod 777 -R /var/www/web_rvec/public/files/
 
 # Bring up application
-php /var/www/html/artisan up
+php /var/www/web_rvec/artisan up
