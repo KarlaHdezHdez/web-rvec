746549ffef27ac35577170e063f9d2fe44038984
perla0212 <115654448+perla0212@users.noreply.github.com>
2023-11-23 11:01:03 -0600

    Update deploy_laravel.sh

--- script/deploy_laravel.sh
+++ script/deploy_laravel.sh
@@ -5,11 +5,11 @@
 
 # Project directory on server for your project.
 export WEB_DIR="/var/www/web-rvec"
-# Your server user. Used to fix permission issue & install our project dependcies
+# Your server user. Used to fix permission issue & install our project dependencies
 export WEB_USER="ubuntu"
 
-# Change directory to project.
-cd $WEB_DIR
+# Change directory to the project.
+cd "$WEB_DIR" || exit 1
 
 # change user owner to ubuntu & fix storage permission issues.
 sudo chown -R ubuntu:ubuntu .
@@ -17,12 +17,12 @@ sudo chown -R www-data storage
 sudo chmod -R u+x .
 sudo chmod g+w -R storage
 
-# install composer dependcies
-sudo -u $WEB_USER composer install --no-dev --no-progress --prefer-dist
+# install composer dependencies with error handling
+sudo -u "$WEB_USER" composer install --no-dev --no-progress --prefer-dist || { echo "Composer installation failed"; exit 1; }
 
 # load .env file from AWS Systems Manager
-script/generate-env.sh
+scripts/generate-env.sh || { echo "Generating .env file failed"; exit 1; }
 
 # generate app key & run migrations
-sudo -u $WEB_USER php artisan key:generate
-sudo -u $WEB_USER php artisan migrate --force --no-interaction
+sudo -u "$WEB_USER" php artisan key:generate || { echo "Generating app key failed"; exit 1; }
+sudo -u "$WEB_USER" php artisan migrate --force --no-interaction || { echo "Running migrations failed"; exit 1; }
