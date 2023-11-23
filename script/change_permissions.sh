e7558658d2d3d738fe8a85bcb15ffe7a352b476c
perla0212 <115654448+perla0212@users.noreply.github.com>
2023-11-23 11:10:34 -0600

    Update change_permissions.sh

--- script/change_permissions.sh
+++ script/change_permissions.sh
@@ -4,6 +4,8 @@
 sudo usermod -a -G apache ec2-user
 sudo chown -R ec2-user:apache /var/www
 sudo chmod 2775 /var/www
+# Make sure the script is executable
+sudo chmod +x scripts/generate-env.sh
 sudo chmod +x script/install_dependencies.sh
 find /var/www -type d -exec sudo chmod 2775 {} \;
 find /var/www -type f -exec sudo chmod 0664 {} \;
