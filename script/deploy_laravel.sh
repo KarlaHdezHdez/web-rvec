a936f20039090a6194c7d320a6e26d9ce6438784
perla0212 <115654448+perla0212@users.noreply.github.com>
2023-11-23 10:38:39 -0600

    Update deploy_laravel.sh

--- script/deploy_laravel.sh
+++ script/deploy_laravel.sh
@@ -1,39 +1,28 @@
 #!/bin/bash
+# Author Emad Zaamout | support@ahtcloud.com
 
-# Enter html directory
-cd /var/www/web_rvec/
+# Runs inside production server.
 
-# Create cache and chmod folders
-mkdir -p /var/www/web_rvec/bootstrap/cache
-mkdir -p /var/www/web_rvec/storage/framework/sessions
-mkdir -p /var/www/web_rvec/storage/framework/views
-mkdir -p /var/www/web_rvec/storage/framework/cache
-mkdir -p /var/www/web_rvec/public/files/
+# Project directory on server for your project.
+export WEB_DIR="/var/www/web-rvec"
+# Your server user. Used to fix permission issue & install our project dependcies
+export WEB_USER="ubuntu"
 
-# Install dependencies
-export COMPOSER_ALLOW_SUPERUSER=1
-composer install -d /var/www/web_rvec/
+# Change directory to project.
+cd $WEB_DIR
 
-# Copy configuration from /var/www/.env, see README.MD for more information
-cp /var/www/.env /var/www/web_rvec/.env
+# change user owner to ubuntu & fix storage permission issues.
+sudo chown -R ubuntu:ubuntu .
+sudo chown -R www-data storage
+sudo chmod -R u+x .
+sudo chmod g+w -R storage
 
-# Migrate all tables
-php /var/www/web_rvec/artisan migrate
+# install composer dependcies
+sudo -u $WEB_USER composer install --no-dev --no-progress --prefer-dist
 
-# Clear any previous cached views
-php /var/www/web_rvec/artisan config:clear
-php /var/www/web_rvec/artisan cache:clear
-php /var/www/web_rvec/artisan view:clear
+# load .env file from AWS Systems Manager
+./devops/scripts/generate-env.sh
 
-# Optimize the application
-php /var/www/web_rvec/artisan config:cache
-php /var/www/web_rvec/artisan optimize
-#php /var/www/html/artisan route:cache
-
-# Change rights
-chmod 777 -R /var/www/web_rvec/bootstrap/cache
-chmod 777 -R /var/www/web_rvec/storage
-chmod 777 -R /var/www/web_rvec/public/files/
-
-# Bring up application
-php /var/www/web_rvec/artisan up
+# generate app key & run migrations
+sudo -u $WEB_USER php artisan key:generate
+sudo -u $WEB_USER php artisan migrate --force --no-interaction
