1cd8f4aeadc82d07e92346cbdfb9394d1e2c27b9
perla0212 <115654448+perla0212@users.noreply.github.com>
2023-11-24 18:41:17 -0600

    Update deploy_laravel.sh

--- script/deploy_laravel.sh
+++ script/deploy_laravel.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
-# Resto del script...
-
-# Runs inside production server.
 
 # Project directory on server for your project.
 export WEB_DIR="/var/www/web-rvec"
@@ -12,20 +9,22 @@ export WEB_USER="ubuntu"
 # Change directory to the project.
 cd "$WEB_DIR" || exit 1
 
-# change user owner to ubuntu & fix storage permission issues.
-sudo chown -R ubuntu:ubuntu   # Cambia el propietario y el grupo
-sudo chmod -R www-data storage
-sudo chmod -R u+x .
-sudo chmod g+w -R storage
+# Change user owner to ubuntu & fix storage permission issues.
+# Change the following line to specify the directory you want to modify.
+# Example: sudo chown -R ubuntu:ubuntu /var/www/web-rvec/public
+sudo chown -R ubuntu:ubuntu /var/www/web-rvec
 
-# install composer dependencies with error handling
+# Fix permissions
+sudo chmod -R u+x .
+sudo chmod -R g+w storage
+
+# Install composer dependencies with error handling
 sudo -u "$WEB_USER" composer install --no-dev --no-progress --prefer-dist || { echo "Composer installation failed"; exit 1; }
 
-# load .env file from AWS Systems Manager
+# Load .env file from AWS Systems Manager
 # Assuming generate-env.sh is in the scripts directory
-./script/generate-env.sh || { echo "generating .env file failed"; exit 1; }
+./script/generate-env.sh || { echo "Generating .env file failed"; exit 1; }
 
-
-# generate app key & run migrations
+# Generate app key & run migrations
 sudo -u "$WEB_USER" php artisan key:generate || { echo "Generating app key failed"; exit 1; }
 sudo -u "$WEB_USER" php artisan migrate --force --no-interaction || { echo "Running migrations failed"; exit 1; }
